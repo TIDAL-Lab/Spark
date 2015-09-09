06e24e024393404e988dd7811cf3d51281edd5d6
Elham Beheshti <beheshti@u.northwestern.edu>
2015-09-09 15:25:07 -0500

    array of components as inputs

--- web/three.js/Spark-webgl/component.js
+++ web/three.js/Spark-webgl/component.js
@@ -9,9 +9,7 @@
  * Circuit-level and the level that shows interactions between electrons and ions as they move through circuit components.
  * This project has been conducted in TIDAL lab (Tangible Interaction Design and Learning Lab) at Northwestern University.
  */
-var electrons, electronGeometry, electronMaterial;
-var i, j;
-var ions, ionGeometry, ionMaterial;
+
 var electronSize = 20, ionSize = 40;
 var rectGeom, rectMesh;
 
@@ -36,10 +34,9 @@ function Component(type, current, res, volt, startx, starty, endx, endy) {
   	}
   	//init();
 
-  	this.init = function() {
+  	this.init = function(electronGeometry, ionGeometry) {
 
   		// create electrons
-		electronGeometry = new THREE.Geometry();
 		for ( i = 0; i < this.electronCount; i ++ ) {
 			// create electrons 
 			var vertex = new THREE.Vector3();
@@ -47,30 +44,22 @@ function Component(type, current, res, volt, startx, starty, endx, endy) {
 			vertex.y = Math.random() * 400 - 200; // between -100 and 100
 			vertex.z = 5;
 
+			//translate the electron to be inside the component
+			var newCoordinate = this.componentToScreen([vertex.x, vertex.y]);
+			vertex.x = newCoordinate[0], vertex.y = newCoordinate[1];
+		    //electrons.geometry.verticesNeedUpdate = true;
 			electronGeometry.vertices.push( vertex );
 		}
-		electronMaterial = new THREE.PointCloudMaterial( { size: electronSize, map: ball, color: 0x000099 , transparent: true } );
-		electrons = new THREE.PointCloud ( electronGeometry, electronMaterial );
-		scene.add ( electrons ); 
 
-		for ( i = 0; i < this.electronCount; i ++) {
-			var electron = electrons.geometry.vertices[i];
-			var newCoordinate = this.componentToScreen([electron.x, electron.y]);
-			electron.x = newCoordinate[0], electron.y = newCoordinate[1];
-		}
-		electrons.geometry.verticesNeedUpdate = true;
+
+		// for ( i = 0; i < this.electronCount; i ++) {
+		// 	var electron = electrons.geometry.vertices[i];
+		// 	var newCoordinate = this.componentToScreen([electron.x, electron.y]);
+		// 	electron.x = newCoordinate[0], electron.y = newCoordinate[1];
+		//  }
+		// electrons.geometry.verticesNeedUpdate = true;
 
 		// create ions
-		ionGeometry = new THREE.Geometry();
-		// for ( i = 0; i < this.ionCount; i ++ ) {
-		// 	// create ions 
-		// 	var vertex = new THREE.Vector3();
-		// 	vertex.x = Math.random() * 1000 - 500; //between -500 and 500
-		// 	vertex.y = Math.random() * 400 - 200; // between -100 and 100
-		// 	vertex.z = 5;
-
-		// 	ionGeometry.vertices.push( vertex );
-		// }
 		var count = 0;
 		for ( i = 1; i < this.l/100; i ++ ) {
 			for (j = 1; j < this.w/100; j++) {
@@ -84,58 +73,19 @@ function Component(type, current, res, volt, startx, starty, endx, endy) {
 				// else { vertex.y = (j+1)*100;} 
 				vertex.z = 5;
 
+				// translate the ion to be inside the component container
+				var newCoordinate = this.componentToScreen([vertex.x, vertex.y]);
+				vertex.x = newCoordinate[0], vertex.y = newCoordinate[1];
+
 				ionGeometry.vertices.push( vertex );
 				count++;
 			}
 		}
 		this.ionCount = count;
 
-		ionMaterial = new THREE.PointCloudMaterial( { size: ionSize, map: ball, color: 0xCC0000 , transparent: true } );
-		ions = new THREE.PointCloud ( ionGeometry, ionMaterial );
-		scene.add ( ions ); 
 
-		// change the coordinate based on the component coordinations
-		for ( i = 0; i < this.ionCount; i ++) {
-			var ion = ions.geometry.vertices[i];
-			var newCoordinate = this.componentToScreen([ion.x, ion.y]);
-			ion.x = newCoordinate[0], ion.y = newCoordinate[1];
-		}
 
-		//this.createContainer();
-				var startX = this.startPoint[0], startY = this.startPoint[1];
-		var endX = this.endPoint[0], endY = this.endPoint[1];
-
-		var rectLength = Math.sqrt((endX - startX)*(endX - startX) + (endY - startY)*(endY - startY));
-		//window.alert(rectLength);
-
-		rectWidth = this.w;
-
-		var rectShape = new THREE.Shape();
-		var v1 = this.componentToScreen([-rectLength/2,-rectWidth/2]);
-		var v2 = this.componentToScreen([-rectLength/2, rectWidth/2]);
-		var v3 = this.componentToScreen([rectLength/2, rectWidth/2]);
-		var v4 = this.componentToScreen([rectLength/2,-rectWidth/2]); 
-		rectShape.moveTo( v1[0],v1[1] );
-		rectShape.lineTo( v2[0],v2[1] );
-		rectShape.lineTo( v3[0],v3[1] );
-		rectShape.lineTo( v4[0],v4[1] );
-		rectShape.lineTo( v1[0],v1[1] );
-		// rectShape.moveTo( -rectLength/2,-rectWidth/2 );
-		// rectShape.lineTo( -rectLength/2, rectWidth/2 );
-		// rectShape.lineTo( rectLength/2, rectWidth/2 );
-		// rectShape.lineTo( rectLength/2, -rectWidth/2 );
-		// rectShape.lineTo( -rectLength/2,-rectWidth/2 );
-
-		rectGeom = new THREE.ShapeGeometry( rectShape );
-		
-		if (this.compType == "wire") {
-			rectMesh = new THREE.Mesh( rectGeom, new THREE.MeshBasicMaterial( { color: 0xCCCC00 } ) ) ; // the box color
-		}
-		else {
-			rectMesh = new THREE.Mesh( rectGeom, new THREE.MeshBasicMaterial( { color: 0xCCCC99 } ) ) ; // the box color
-		}		
-
-		scene.add( rectMesh );
+		this.createContainer();
 	}
 
 	// draw the rectangle behind the component
--- web/three.js/Spark-webgl/main.js
+++ web/three.js/Spark-webgl/main.js
@@ -15,8 +15,10 @@ if ( ! Detector.webgl ) Detector.addGetWebGLMessage();
 var container;
 var camera, scene, renderer;
 var mouseX = 0, mouseY = 0;
-var ball; //image element for particles
+var sphere; //image element for particles
 var components = []; // an array of components
+var electrons, electronGeometry, electronMaterial;
+var ions, ionGeometry, ionMaterial;
 
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
@@ -26,7 +28,7 @@ animate();
 
 function init() {
 	
-	ball = THREE.ImageUtils.loadTexture( "textures/ball.png" );
+	sphere = THREE.ImageUtils.loadTexture( "textures/ball.png" );
 	container = document.createElement( 'div' );
 	document.body.appendChild( container );
 
@@ -37,11 +39,13 @@ function init() {
 	scene.fog = new THREE.FogExp2( 0xffffff, 0.0007 );
 	
 	THREE.ImageUtils.crossOrigin = 'anonymous'; // for using images from the image folder
-	var comp1 = new Component("wire", 0, 1, 0, 400, 300, -400, -300);
+	// component inputs (type, current, resistance, volt, startx, starty, endx, endy)
+	var comp1 = new Component("wire", 0, 1, 0, 400, 300, -400, -300); 
+	//var comp1 = new Component("wire", 0, 1, 0, -1010, 0, -10, 0);
 	components.push(comp1);
-	var comp2 = new Component("resistor", 0, 1, 0, 10, 0, 1010, 0);
+	var comp2 = new Component("wire", 0, 1, 0, 10, 0, 1010, 0);
 	components.push(comp2);
-	console.log(components.length);
+	console.log('# of components = ' + components.length);
 	
 	initComponents();
 
@@ -89,16 +93,22 @@ function render() {
 }
 
 function initComponents() {
-	for (i=0; i < components.length; i++) {
-		var component = components[i];
-		
-		//component.createContainer();
-		if (i == 0) {
-			console.log("do you see me?");
-		}
-		component.init();
-
+	electronGeometry = new THREE.Geometry();
+	ionGeometry = new THREE.Geometry();
+	for (k=0; k < components.length; k++) {
+		console.log(k);
+		components[k].init(electronGeometry, ionGeometry);	
+		console.log(k);
 	}
+	console.log('# of electrons = ' + electronGeometry.vertices.length);
+	console.log('# of ions = ' + ionGeometry.vertices.length);
+	electronMaterial = new THREE.PointCloudMaterial( { size: electronSize, map: sphere, color: 0x000099 , transparent: true } );
+	electrons = new THREE.PointCloud ( electronGeometry, electronMaterial );
+	scene.add ( electrons ); 
+
+	ionMaterial = new THREE.PointCloudMaterial( { size: ionSize, map: sphere, color: 0xCC0000 , transparent: true } );
+	ions = new THREE.PointCloud ( ionGeometry, ionMaterial );
+	scene.add ( ions ); 
 }
 
 function updateElectrons() {
