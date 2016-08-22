49bd553566c24053616b039b1e340afd87ce9b11
ebehesht <beheshti@u.northwestern.edu>
2016-08-22 02:15:49 -0500

    testing different markers

--- web/three.js/ar-spark.js
+++ web/three.js/ar-spark.js
@@ -24,32 +24,32 @@ function JsArInit() {
 		arScale = 100;
 		markerID = 56;
 		// Testing Barcode marker: See artoolkit5/doc/patterns/Matrix code 3x3 (72dpi)/20.png
-		markerRootParent = arController.createThreeBarcodeMarker(markerID, 1);
+/*		markerRootParent = arController.createThreeBarcodeMarker(markerID, 1);
 		markerRoot = new THREE.Mesh();
 		initComponents();
 		markerRootParent.add(markerRoot);
 		scene.add(markerRootParent);
 
-		arController.setPatternDetectionMode(artoolkit.AR_MATRIX_CODE_DETECTION);
+		arController.setPatternDetectionMode(artoolkit.AR_MATRIX_CODE_DETECTION);*/
 
-		// markerRoot = new THREE.Mesh();
-		// var markerLoaded = false;
-		// // Testing Pattern marker:
-		// arController.loadMarker('lib/jsartoolkit5-master/examples/Data/patt.hiro', function(markerId) {
-		// 	markerLoaded = true;
-		// 	markerRootParent = arController.createThreeMarker(markerId);			
-		// 	initComponents();
-		// 	markerRootParent.add(markerRoot);
-		// 	scene.add(markerRootParent);
-		// });
+		markerRoot = new THREE.Mesh();
+		var markerLoaded = false;
+		// Testing Pattern marker:
+		arController.loadMarker('markers/plus16.pat', function(markerId) {
+			markerLoaded = true;
+			console.log(markerId);
+			markerID = markerId;
+			markerRootParent = arController.createThreeMarker(markerId);			
+			initComponents();
+			markerRootParent.add(markerRoot);
+			scene.add(markerRootParent);
+		});
 
 		//EB: set width and height of renderer
 		arController.videoWidth = modelWidth;
 		arController.videoHeight = modelHeight;
 
-		document.body.className = arController.orientation;
-
-		
+		document.body.className = arController.orientation;		
 
 		var renderer = new THREE.WebGLRenderer({antialias: true});
 		renderer.setClearColor ( backgroundBlue ); 			//bluish background color
@@ -68,33 +68,23 @@ function JsArInit() {
 		//controls = new THREE.OrbitControls( camera, renderer.domElement );
 		controls = new THREE.EditorControls( camera, renderer.domElement );
 
-		// renderer.domElement.addEventListener('click', function(ev) {
-		// 	ev.preventDefault();
-		// 	console.log("touched");
-		// }, false);
 		// arController.setProjectionNearPlane(1);
 		// arController.setProjectionFarPlane(10000);
-		console.log("near", arController.getProjectionNearPlane());
-		console.log("far", arController.getProjectionFarPlane());
+		// console.log("near", arController.getProjectionNearPlane());
+		// console.log("far", arController.getProjectionFarPlane());
 
 		// if a marker is detected, enable updatingElectrons() to fire.
 		arController.addEventListener('getMarker', function(ev) {
 			if (ev.data.marker.id == markerID) {   
 				markerDetectedFlag = true;
-				//console.log("camera: ", camera.position);
-				//console.log("controller getcameramatrix: ", arController.getCameraMatrix());
-				//console.log("Detected marker with ids:", ev.data.marker.id, ev.data.marker.idPatt, ev.data.marker.idMatrix);
-				//console.log("Marker data", ev.data.marker);
-				//console.log("Marker transform matrix:", [].join.call(ev.data.matrix, ', '));
 			}
 		});
 
 		var tick = function() {
-			//if (markerLoaded) {   // the flag is for using pattern markers
+			if (markerLoaded) {   // the flag is for using pattern markers
 				markerRoot.scale.set(1/arScale, 1/arScale, 1/arScale);
 				electronVertices.material.size /= arScale;
-				markerRoot.updateMatrixWorld();
-				//markerRoot.matrixWorldNeedsUpdate = true;
+				markerRoot.updateMatrixWorld();				
 				markerDetectedFlag = false;
 				if (!freezeFlag) arScene.process();
 				arScene.renderOn(renderer);
@@ -102,10 +92,9 @@ function JsArInit() {
 					markerRoot.scale.set(1, 1, 1);
 					electronVertices.material.size *= arScale;
 					markerRoot.updateMatrixWorld();
-					//markerRoot.matrixWorldNeedsUpdate = true;
 	        		if (freezeFlag || markerDetectedFlag) updateElectrons(); // update electrons only when a marker is detected or if it is freezed
 	    		}
-	    	//}
+	    	}
 			requestAnimationFrame(tick);
 		};
 
